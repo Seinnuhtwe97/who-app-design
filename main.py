1bfb1ced6d0d90d08415c2a3f908e80e021de29d
Seinnuhtwe97 <seinnuhtwe@gmail.com>
2018-08-26 16:14:42 +0630

    added

--- main.py
+++ main.py
@@ -2,33 +2,33 @@ from flask import Flask, render_template, url_for
 
 myapp = Flask(__name__)
 
-@myapp.route("/index.html")
+@myapp.route("/index")
 def home():
     return render_template("index.html")
 
 
-@myapp.route("/political.html")
+@myapp.route("/political")
 def political():
     return render_template("political.html")
 
 
 
-@myapp.route("/man.html")
+@myapp.route("/man")
 def man():
     return render_template("man.html")
 
 
-@myapp.route("/writer.html")
+@myapp.route("/writer")
 def writer():
     return render_template("writer.html")
 
 
-@myapp.route("/actor.html")
+@myapp.route("/actor")
 def actor():
     return render_template("actor.html")
 
 
-@myapp.route("/designer.html")
+@myapp.route("/designer")
 def designer():
     return render_template("designer.html")
 
