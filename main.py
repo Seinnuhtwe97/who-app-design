6b631231d8f5678aaae697487fc5cac7fe5d9ba7
Seinnuhtwe97 <seinnuhtwe@gmail.com>
2018-08-26 15:32:00 +0630

    added

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for
+
+myapp = Flask(__name__)
+
+@myapp.route("/")
+def home():
+    return render_template("index.html")
+
+
+@myapp.route("/politican")
+def home():
+    return render_template("political.html")
+
+
+
+@myapp.route("/business")
+def home():
+    return render_template("man.html")
+
+
+@myapp.route("/writer")
+def home():
+    return render_template("writer.html")
+
+
+@myapp.route("/actor")
+def home():
+    return render_template("actor.html")
+
+
+@myapp.route("/designer")
+def home():
+    return render_template("designer.html")
+
+if __name__ =="__main__":
+    myapp.run(debug=True)
