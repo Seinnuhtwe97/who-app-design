a83d77658b69b5baa3b1496921e932aeb755a8c5
Seinnuhtwe97 <seinnuhtwe@gmail.com>
2018-08-26 19:29:40 +0630

    added

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
-myapp = Flask(__name__)
-@myapp.route('/index')
+app = Flask(__name__)
+@myapp.route("/index")
 def index():
     return render_template("index.html")
 	
-@myapp.route('/political')
+@myapp.route("/political")
 def political():
 		return render_template("political.html")
 
-@myapp.route('/man')
+@myapp.route("/man")
 def man():
     return render_template("man.html")
 
-@myapp.route('/writer')
+@myapp.route("/writer")
 def writer():
     return render_template("writer.html")
 
-@myapp.route('/actor')
+@myapp.route("/actor")
 def actor():
     return render_template("actor.htm")
 
-@myapp.route('/designer')
+@myapp.route("/designer")
 def designer():
     return render_template("designer.html")
 
 if __name__ == "__main__":
-    myapp.route(Debug=True)
+    app.route(Debug=True)
