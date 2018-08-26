abd8f0797f67a7b92e00326765f0f797ba99c193
Seinnuhtwe97 <seinnuhtwe@gmail.com>
2018-08-26 17:48:27 +0630

    added

--- main.py
+++ main.py
@@ -3,27 +3,27 @@ myapp = Flask(__name__)
 
 @myapp.route("/")
 def hello():
-    return render.template("index.html")
+    return render_template("index.html")
 
 
 @myapp.route("/man")
 def man():
-    return render.template("man.html")
+    return render_template("man.html")
 
 
 @myapp.route("/writer")
 def writer():
-    return render.template("writer.html")
+    return render_template("writer.html")
 
 
 @myapp.route("/actor")
 def actor():
-    return render.template("actor.html")
+    return render_template("actor.html")
 
 
 @myapp.route("/designer")
 def designer():
-    return render.template("designer.html")
+    return render_template("designer.html")
 
 if __name__ == "__main__":
     myapp.route(Debug=True)
